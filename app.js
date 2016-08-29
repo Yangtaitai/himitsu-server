4e0ebfffd0dbb3130f3d6f0a4f05dcc8faa4d9dc
Yangtaitai <amyfan@gwmail.gwu.edu>
2016-08-29 10:47:23 -0400

    Revert "imageUpload api"
    This reverts commit 109effdd9daf67561a5096e1f41f627500edaf37.

--- app.js
+++ app.js
@@ -5,28 +5,17 @@ var MongoStore = require('connect-mongo')(session);
 var config = require('./config');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var path = require('path');
-
 
 var himitusPassport = require('./plugins/passport.js');
 
 var app = express();
 
 var cors = require('cors');
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+app.use(cors({origin:true,
+    credentials:true}));
 
 //set all Post data into req.body
 app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-//app.use(express.static(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
     secret: config.session_secret,
@@ -43,5 +32,5 @@ himitusPassport(app);
 routes(app);
 
 var server = app.listen(4000, function() {
-    console.log('Express server listening on port ' + server.address().port);
+  console.log('Express server listening on port ' + server.address().port);
 });
--- routes.js
+++ routes.js
@@ -1,49 +1,45 @@
+
 var userRoute = require('./routes/userRoute');
 var secretRoute = require('./routes/secretRoute');
 var commentRoute = require('./routes/commentRoute');
-var imageRoute = require('./routes/imageRoute');
-var multer = require('multer');
-var upload = multer({ dest: './public/upload' });
-
-module.exports = function(app) {
 
 
-    app.post('/login', userRoute.login);
-
+module.exports = function(app){
+    
+    
+    app.post('/login',userRoute.login);
+    
     //user
-
-    app.post('/user', userRoute.createUser); //sign up
+    
+    app.post('/user',userRoute.createUser);   //sign up
 
     app.get('/user', userRoute.getUserList);
-
+    
     app.get('/user/:id', userRoute.ensureAuthenticated, userRoute.getUser);
-
-    app.put('/user/:id', userRoute.updateUser);
-
-    app.delete('/user/:id', userRoute.deleteUser);
-
+    
+    app.put('/user/:id',userRoute.updateUser);
+        
+    app.delete('/user/:id',userRoute.deleteUser);
+    
     //secret
-
-    app.get('/secret', secretRoute.getSecretList);
-
-    app.get('/secret/:id', secretRoute.getSecret);
-
-    app.post('/secret', secretRoute.createSecret);
-
-    app.put('/secret/:id', secretRoute.updateSecret);
-
+    
+    app.get('/secret',secretRoute.getSecretList);
+    
+    app.get('/secret/:id',secretRoute.getSecret);
+    
+    app.post('/secret',secretRoute.createSecret);
+    
+    app.put('/secret/:id',secretRoute.updateSecret);
+    
     app.delete('/secret/:id', secretRoute.deleteSecret);
-
-    //image upload filename
-    app.post('/upload', upload.single('file'), imageRoute.upload);
-
+    
     //comment
-
-    app.get('/comment', commentRoute.getCommentList);
-
-    app.get('/comment/:id', commentRoute.getComment);
-
-    app.post('/comment', commentRoute.createComment);
-
+    
+    app.get('/comment',commentRoute.getCommentList);
+    
+    app.get('/comment/:id',commentRoute.getComment);
+    
+    app.post('/comment',commentRoute.createComment);
+    
     app.delete('comment/:id', commentRoute.deleteComment);
 }
